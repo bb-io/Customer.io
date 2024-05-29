1f19c9a938256583ebf1d6f83f79f0e27a729e45
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-29 11:03:33 +0300

    Fixed deserialization error

--- Apps.Customer.io/Models/Response/EmailHeader.cs
+++ Apps.Customer.io/Models/Response/EmailHeader.cs
@@ -1,8 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Apps.Customer.io.Models.Response;
 
 public class EmailHeader
 {
+    [JsonProperty("name")]
     public string Name { get; set; }
 
+    [JsonProperty("value")]
     public string Value { get; set; }
 }
---
+++ Apps.Customer.io/Utils/Converters/EmailHeaderListConverter.cs
@@ -0,0 +1,39 @@
+﻿using Apps.Customer.io.Models.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Apps.Customer.io.Utils.Converters;
+
+public class EmailHeaderListConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(List<EmailHeader>);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        var token = JToken.Load(reader);
+        if (token.Type == JTokenType.String)
+        {
+            if (token.ToString() == "[]")
+            {
+                return new List<EmailHeader>();
+            }
+            
+            return JsonConvert.DeserializeObject<List<EmailHeader>>(token.ToString())!;
+        }
+
+        if (token.Type == JTokenType.Array)
+        {
+            return token.ToObject<List<EmailHeader>>()!;
+        }
+
+        throw new JsonSerializationException("Unexpected token type for EmailHeader list");
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        serializer.Serialize(writer, value);
+    }
+}
