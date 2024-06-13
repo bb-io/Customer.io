04132a404566583434484982518fefc0119d0e5e
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-06-13 18:25:30 +0300

    Added file to response of newsletter actions

--- Apps.Customer.io/Actions/NewslettersActions.cs
+++ Apps.Customer.io/Actions/NewslettersActions.cs
@@ -1,46 +1,58 @@
+using System.Text;
 using Apps.Customer.io.Api;
 using Apps.Customer.io.Constants;
 using Apps.Customer.io.Invocables;
-using Apps.Customer.io.Models.Entity;
 using Apps.Customer.io.Models.Request.Newsletter;
 using Apps.Customer.io.Models.Response.Newsletter;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Blackbird.Applications.Sdk.Utils.Extensions.Http;
 using RestSharp;
 
 namespace Apps.Customer.io.Actions;
 
 [ActionList]
-public class NewslettersActions : CustomerIoInvocable
+public class NewslettersActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+    : CustomerIoInvocable(invocationContext)
 {
-    public NewslettersActions(InvocationContext invocationContext) : base(invocationContext)
-    {
-    }
-
     [Action("Get translation of a newsletter",
         Description = "Get information about a translation of an individual newsletter")]
-    public async Task<NewsletterTranslationEntity> GetNewsletterTranslation(
+    public async Task<NewsletterTranslationFileResponse> GetNewsletterTranslation(
         [ActionParameter] NewsletterRequest input)
     {
-        var endpoint = $"v1/newsletters/{input.NewsletterId}/language/{input.Language}";
-        var request = new CustomerIoRequest(endpoint, Method.Get, Creds);
-
-        var response = await Client.ExecuteWithErrorHandling<NewsletterTranslationResponse>(request);
-        return response.Content;
+        return await HandleNewsletterTranslation(input, Method.Get);
     }
 
     [Action("Update translation of a newsletter", Description = "Update the translation of a newsletter variant")]
-    public async Task<NewsletterTranslationEntity> UpdateNewsletterTranslation(
+    public async Task<NewsletterTranslationFileResponse> UpdateNewsletterTranslation(
         [ActionParameter] NewsletterRequest input,
         [ActionParameter] UpdateNewsletterTranslationRequest payload)
     {
+        return await HandleNewsletterTranslation(input, Method.Put, payload);
+    }
+
+    private async Task<NewsletterTranslationFileResponse> HandleNewsletterTranslation(
+        NewsletterRequest input, 
+        Method method, 
+        UpdateNewsletterTranslationRequest? payload = null)
+    {
         var endpoint = $"v1/newsletters/{input.NewsletterId}/language/{input.Language}";
-        var request = new CustomerIoRequest(endpoint, Method.Put, Creds)
-            .WithJsonBody(payload, JsonConfig.Settings);
+        var request = new CustomerIoRequest(endpoint, method, Creds);
+
+        if (payload != null)
+        {
+            request.WithJsonBody(payload, JsonConfig.Settings);
+        }
 
         var response = await Client.ExecuteWithErrorHandling<NewsletterTranslationResponse>(request);
-        return response.Content;
+        
+        var html = response.Content.Body;
+        await using var stream = new MemoryStream(Encoding.UTF8.GetBytes(html));
+        var contentType = "text/html";
+        var fileReference = await fileManagementClient.UploadAsync(stream, contentType, $"{response.Content.Id}.html");
+        
+        return new NewsletterTranslationFileResponse(response.Content, fileReference);
     }
 }
---
+++ Apps.Customer.io/Models/Response/Newsletter/NewsletterTranslationFileResponse.cs
@@ -0,0 +1,28 @@
+﻿using Apps.Customer.io.Models.Entity;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.Customer.io.Models.Response.Newsletter;
+
+public class NewsletterTranslationFileResponse : NewsletterTranslationEntity
+{
+    public FileReference File { get; set; }
+    
+    public NewsletterTranslationFileResponse(NewsletterTranslationEntity entity, FileReference file)
+    {
+        File = file;
+        this.Id = entity.Id;
+        this.NewsletterId = entity.NewsletterId;
+        this.DeduplicateId = entity.DeduplicateId;
+        this.Name = entity.Name;
+        this.Layout = entity.Layout;
+        this.Body = entity.Body;
+        this.BodyAmp = entity.BodyAmp;
+        this.Language = entity.Language;
+        this.Type = entity.Type;
+        this.From = entity.From;
+        this.FromId = entity.FromId;
+        this.ReplyTo = entity.ReplyTo;
+        this.ReplyToId = entity.ReplyToId;
+        this.Preprocessor = entity.Preprocessor;
+    }
+}
