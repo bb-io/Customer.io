9912fc5e83e526e3e59a201cda119a984de21b5c
RiabushenkoA <ariabushenko@blackbird.io>
2024-12-11 18:33:52 +0200

    new event

---
+++ Apps.Customer.io/Actions/Base/BasePollingAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Customer.io.Api;
+using Apps.Customer.io.Invocables;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+
+namespace Apps.Customer.io.Actions.Base
+{
+    public abstract class BasePollingAction : CustomerIoInvocable
+    {
+        protected readonly CustomerIoClient Client;
+
+        protected BasePollingAction(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+        : base(invocationContext)
+        {
+            Client = new CustomerIoClient(invocationContext.AuthenticationCredentialsProviders.ToArray());
+        }
+    }
+}
---
+++ Apps.Customer.io/Polling/Models/BroadcastEventResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Customer.io.Polling.Models
+{
+    public class BroadcastEventResponse
+    {
+        public List<Broadcast> Broadcasts { get; set; }
+    }
+
+    public class Broadcast
+    {
+        public int Id { get; set; }
+        public string DeduplicateId { get; set; }
+        public long Created { get; set; }
+        public string Type { get; set; }
+        public long Updated { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public string State { get; set; }
+        public List<string> Actions { get; set; }
+        public List<string> Tags { get; set; }
+        public long FirstStarted { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
