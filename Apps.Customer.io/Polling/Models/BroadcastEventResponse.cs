ec6f82eaec3e510281bc100afc17a67d3ffdec97
RiabushenkoA <ariabushenko@blackbird.io>
2024-12-11 18:51:20 +0200

    fixes

--- Apps.Customer.io/Polling/Models/BroadcastEventResponse.cs
+++ Apps.Customer.io/Polling/Models/BroadcastEventResponse.cs
@@ -1,29 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 
 namespace Apps.Customer.io.Polling.Models
 {
     public class BroadcastEventResponse
     {
+        [Display("Broadcasts")]
+        [JsonProperty("broadcasts")]
         public List<Broadcast> Broadcasts { get; set; }
     }
 
     public class Broadcast
     {
+        [Display("Broadcast ID")]
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("deduplicate_id")]
         public string DeduplicateId { get; set; }
+
+        [JsonProperty("created")]
         public long Created { get; set; }
+
+        [JsonProperty("type")]
         public string Type { get; set; }
+
+        [JsonProperty("updated")]
         public long Updated { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("active")]
         public bool Active { get; set; }
+
+        [JsonProperty("state")]
         public string State { get; set; }
+
+        [JsonProperty("actions")]
         public List<string> Actions { get; set; }
+
+        [JsonProperty("tags")]
         public List<string> Tags { get; set; }
+
+        [JsonProperty("first_started")]
         public long FirstStarted { get; set; }
+
+        [JsonProperty("created_by")]
         public string CreatedBy { get; set; }
     }
 }
