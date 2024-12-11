93e6b0553b5fb48740c067fba715eea3439677b7
RiabushenkoA <ariabushenko@blackbird.io>
2024-12-11 19:37:29 +0200

    fixes

---
+++ Apps.Customer.io/Polling/Models/NewsletterEventResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json.Serialization;
+using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
+
+namespace Apps.Customer.io.Polling.Models
+{
+    public class NewsletterEventResponse
+    {
+        [Display("Newsletters")]
+        [JsonProperty("newsletters")]
+        public List<Newsletter> Newsletters { get; set; }
+
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+    }
+
+    public class Newsletter
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("deduplicate_id")]
+        public string DeduplicateId { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("created")]
+        public long Created { get; set; }
+
+        [JsonProperty("updated")]
+        public long Updated { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+
+        [JsonProperty("subscription_topic_id")]
+        public int SubscriptionTopicId { get; set; }
+    }
+
+}
---
+++ Apps.Customer.io/Polling/NewsletterPollingList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Customer.io.Actions.Base;
+using Apps.Customer.io.Api;
+using Apps.Customer.io.Polling.Models;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+using RestSharp;
+
+namespace Apps.Customer.io.Polling
+{
+    [PollingEventList]
+    public class NewsletterPollingList(InvocationContext invocationContext): BasePollingAction(invocationContext, null)
+    {
+        [PollingEvent("On newsletter created or updated", "Triggered when a newsletter is created or updated")]
+        public async Task<PollingEventResponse<NewsletterMemory, NewsletterEventResponse>> OnNewsletterCreatedOrUpdated(
+           PollingEventRequest<NewsletterMemory> request)
+        {
+            if (request.Memory is null)
+            {
+                return new PollingEventResponse<NewsletterMemory, NewsletterEventResponse>
+                {
+                    FlyBird = false,
+                    Memory = new NewsletterMemory
+                    {
+                        LastPollingTime = DateTime.UtcNow,
+                        Triggered = false
+                    }
+                };
+            }
+
+            var lastPollingTime = request.Memory.LastPollingTime ?? DateTime.MinValue;
+
+            var newsletters = new List<Newsletter>();
+            string nextToken = null;
+            do
+            {
+                var requestClient = new CustomerIoRequest(
+                    $"v1/newsletters?limit=100{(nextToken != null ? $"&start={nextToken}" : "")}",
+                    Method.Get,
+                    InvocationContext.AuthenticationCredentialsProviders
+                );
+                var response = await Client.ExecuteWithErrorHandling<NewsletterEventResponse>(requestClient);
+
+                if (response?.Newsletters != null)
+                    newsletters.AddRange(response.Newsletters);
+
+                nextToken = response?.Next;
+            } while (!string.IsNullOrEmpty(nextToken));
+
+            var newNewsletters = newsletters
+                .Where(n => DateTimeOffset.FromUnixTimeSeconds(n.Created) > lastPollingTime)
+                .ToList();
+
+            var updatedNewsletters = newsletters
+                .Where(n => DateTimeOffset.FromUnixTimeSeconds(n.Updated) > lastPollingTime && n.Created != n.Updated)
+                .ToList();
+
+            if (newNewsletters.Any() || updatedNewsletters.Any())
+            {
+                var latestEventTime = newsletters
+                    .Max(n => DateTimeOffset.FromUnixTimeSeconds(n.Updated).UtcDateTime);
+
+                return new PollingEventResponse<NewsletterMemory, NewsletterEventResponse>
+                {
+                    FlyBird = true,
+                    Memory = new NewsletterMemory
+                    {
+                        LastPollingTime = latestEventTime,
+                        Triggered = true
+                    },
+                    Result = new NewsletterEventResponse
+                    {
+                        Newsletters = newNewsletters.Concat(updatedNewsletters).ToList()
+                    }
+                };
+            }
+
+            return new PollingEventResponse<NewsletterMemory, NewsletterEventResponse>
+            {
+                FlyBird = false,
+                Memory = request.Memory
+            };
+        }
+    }
+
+}
+
