0c3397fd0c058985aa7e31bd4977b169b814eab7
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-24 16:37:07 +0200

    add CampaignTagsDataHandler, update tests

--- Apps.Customer.io/Models/Request/Campaign/SearchCampaignsRequest.cs
+++ Apps.Customer.io/Models/Request/Campaign/SearchCampaignsRequest.cs
@@ -1,4 +1,6 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Customer.io.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Customer.io.Models.Request.Campaign;
 
@@ -7,5 +9,6 @@ public class SearchCampaignsRequest
     [Display("Name contains")]
     public string? NameContains { get; set; }
 
+    [Display("Tags"), DataSource(typeof(CampaignTagsDataHandler))]
     public IEnumerable<string>? Tags { get; set; }
 }
